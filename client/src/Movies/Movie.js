ed1c46c1540979a9bac30465d415825765a131ca
gconnell29 <gconnelljr29@gmail.com>
2021-02-11 01:57:19 -0500

    obtained id using useParams, linked home link to / path

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {useParams} from 'react-router-dom';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
-
-  let id = 1;
+  console.log(`params:`, useParams());
+  let {id} = useParams();
+  // console.log(id);
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
